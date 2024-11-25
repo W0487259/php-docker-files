cfc3ab453505b36a1a7295cb2047566ee08f9078
Vanoostrum <w0487259@nscc.ca>
2024-11-25 10:14:22 -0400

    Created the database and table for A4, began writing the form processor file.

---
+++ app/src/A4/createA4Tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `registered_users` (
+    `user_id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `firstName` varchar(50) NOT NULL,
+    `lastName` varchar(50) NOT NULL,
+    `street` varchar(100) NOT NULL,
+    `city` varchar(100) NOT NULL,
+    `province` varchar(30) NOT NULL,
+    `postalCode` varchar(11) NOT NULL,
+    `country` varchar(6) NOT NULL,
+    `phone` varchar(15) NOT NULL,
+    `email` varchar(150) NOT NULL,
+    `subscribed` bool NOT NULL
+);
+
---
+++ app/src/A4/insert_user.php
@@ -0,0 +1,44 @@
+<?php
+include("conn.php");
+
+/**
+ * Insert Registered User File
+ * Author: Evan van Oostrum
+ * Date: 11/25/2024
+ * 
+ * Last edited: 11/25/2024
+ * Filename: insert_user.php
+ */
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo "Connecting to the database server...<br/>";
+
+    $campus = htmlspecialchars($_POST["campus"]);
+    $building = htmlspecialchars($_POST["building"]);
+    $roomNumber = htmlspecialchars($_POST["roomNumber"]);
+
+
+    $conn = new mysqli($host, $user, $pass, $name);
+    if(!$conn) { // Check if the connection is valid
+        die("Connection failed: " + mysqli_connect_error());
+    }
+
+    echo "Successfully connected to the server!";
+
+    // Prepare SQL statement and bind (bind parameters are used to help prevent SQL injections)
+    $stmt = $conn->prepare("INSERT INTO Room (Campus, Building, RoomNumber) VALUES (?, ?, ?);");
+    $stmt->bind_param("sss", $campus, $building, $roomNumber);
+
+    echo "<br>" . $campus . " - " . $building . " - " . $roomNumber . "<br>";
+
+    $result = $stmt->execute();
+    if($result) {
+        echo "<h3>Success!</h3>";
+    } else {
+        echo "Error: Unable to insert room :(";
+    }
+
+    $conn->close();
+    echo "<p><a href='./view_room.php'>View Rooms</a></p>";
+
+}
