1428ac4d50a5bba40d8532959980bf661e3bb0ab
Vanoostrum <w0487259@nscc.ca>
2024-11-27 09:06:40 -0400

    Created the user inserts, haven't tested yet because I need to make sure booleans work.

--- app/src/A4/insert_user.php
+++ app/src/A4/insert_user.php
@@ -6,16 +6,25 @@ include("conn.php");
  * Author: Evan van Oostrum
  * Date: 11/25/2024
  * 
- * Last edited: 11/25/2024
+ * Last edited: 11/27/2024
  * Filename: insert_user.php
  */
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "Connecting to the database server...<br/>";
 
-    $campus = htmlspecialchars($_POST["campus"]);
-    $building = htmlspecialchars($_POST["building"]);
-    $roomNumber = htmlspecialchars($_POST["roomNumber"]);
+    // Grabs all 10 of the user inputs
+    $firstName = htmlspecialchars($_POST["firstName"]);
+    $lastName = htmlspecialchars($_POST["lastName"]);
+    $street = htmlspecialchars($_POST["street"]);
+    $city = htmlspecialchars($_POST["city"]);
+    $province = htmlspecialchars($_POST["province"]);
+    $postalCode = htmlspecialchars($_POST["postalCode"]);
+    $country = htmlspecialchars($_POST["country"]);
+    $phone = htmlspecialchars($_POST["phone"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $subscribed = $_POST["newsletter"];
+    
 
 
     $conn = new mysqli($host, $user, $pass, $name);
@@ -26,19 +35,34 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "Successfully connected to the server!";
 
     // Prepare SQL statement and bind (bind parameters are used to help prevent SQL injections)
-    $stmt = $conn->prepare("INSERT INTO Room (Campus, Building, RoomNumber) VALUES (?, ?, ?);");
-    $stmt->bind_param("sss", $campus, $building, $roomNumber);
+    $stmt = $conn->prepare(
+        "INSERT INTO User (FirstName, LastName, Street, City, Province, 
+            PostalCode, Country, Phone, Email, Newsletter) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
+    );
+    $stmt->bind_param("sssssssssb", $firstName, $lastName, $street,
+        $city, $province, $postalCode, $country, $phone, $email, $subscribed);
 
-    echo "<br>" . $campus . " - " . $building . " - " . $roomNumber . "<br>";
+    echo "<br>First name: " . $firstName . "<br>"
+        . "Last name: " . $lastName . "<br>"
+        . "Street: " . $street . "<br>"
+        . "City: " . $city . "<br>"
+        . "Province: " . $province . "<br>"
+        . "Postal Code: " . $postalCode . "<br>"
+        . "Country: " . $country . "<br>"
+        . "Phone: " . $phone . "<br>"
+        . "Email: " . $email . "<br>"
+        . "Is Subscribed: " . $subscribed . "<br>"
+        . "<br>";
 
     $result = $stmt->execute();
     if($result) {
         echo "<h3>Success!</h3>";
     } else {
-        echo "Error: Unable to insert room :(";
+        echo "Error: Unable to insert user :(";
     }
 
     $conn->close();
-    echo "<p><a href='./view_room.php'>View Rooms</a></p>";
-
+} else {
+    echo "Error: You probably 'forgot' to press submit, right?";
 }
